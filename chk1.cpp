9ec69d2581b4db4595eefa85f8be055a289dd209
alonc-dev <alonc.dev@gmail.com>
2024-08-20 16:30:31 +0300

    try1 ready

--- chk1.cpp
+++ chk1.cpp
@@ -1,7 +1,22 @@
+#include <stdio.h>
 #include <iostream>
 using namespace std;
 
+int MyFun1(int a, int b);
+
 int main(){
     cout << "Hello World 4 !" << endl;
+
+    for (int i=0; i<10; i++){
+        int c = MyFun1(i,i);
+        printf("Count: %d: %d\n",i, c);
+    }
+    printf("Hello World 5\n");
+
     return 0;
+}
+
+
+int MyFun1(int a, int b){
+    return a+b;
 }
